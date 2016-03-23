662d061a9b9d6979917df2deabc9f9c3c2c41680
gchandrakanth89 <Pervacio@PVCO165>
2016-03-23 19:50:19 +0530

    Second commit

--- Git_P1/src/com/example/git_p1/MainActivity.java
+++ Git_P1/src/com/example/git_p1/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.git_p1;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+	private static final String tag = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.d(tag, "Hello Hi");
 	}
 }
