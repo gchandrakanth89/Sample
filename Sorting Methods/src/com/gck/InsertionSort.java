feb50350c64084dbf186c6b6df9997c4e20a5886
gchandrakanth89 <Pervacio@PVCO165>
2016-04-06 12:15:40 +0530

    Initial commit-Insertion sort

---
+++ Sorting Methods/src/com/gck/InsertionSort.java
@@ -0,0 +1,30 @@
+package com.gck;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+	public static int[] insertionSort(int[] input) {
+
+		int i = 1;
+		while (i < input.length) {
+
+			for (int j = 0; j < i; j++) {
+				if (input[i] < input[j]) {
+					// swap
+					int temp = input[i];
+					input[i] = input[j];
+					input[j] = temp;
+				}
+			}
+			i++;
+		}
+
+		return input;
+	}
+
+	public static void main(String[] args) {
+		int a[] = { 5, 2, 4, 6, 1, 3 };
+		int[] insertionSort = insertionSort(a);
+		System.out.println(Arrays.toString(insertionSort));
+	}
+}
