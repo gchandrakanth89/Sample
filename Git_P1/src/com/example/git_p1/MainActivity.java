1e157df9da81a28c3f4d6351dc88ff37eb4cb056
gchandrakanth89 <Pervacio@192.168.0.2>
2016-04-12 22:42:47 +0530

    Second commit

--- Git_P1/src/com/example/git_p1/MainActivity.java
+++ Git_P1/src/com/example/git_p1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.git_p1;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +17,30 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Log.d(tag, "Hello Hi");
+		Button button1 = (Button) findViewById(R.id.button1);
+		Button button2 = (Button) findViewById(R.id.button2);
+		button1.setOnClickListener(clickListener);
+		button2.setOnClickListener(clickListener);
+
 	}
+
+	View.OnClickListener clickListener = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+
+			FragmentManager fragmentManager = getFragmentManager();
+			FragmentTransaction transaction = fragmentManager.beginTransaction();
+			switch (v.getId()) {
+			case R.id.button1:
+				transaction.replace(R.id.frag_container, new FragmentOne());
+				break;
+			case R.id.button2:
+				transaction.replace(R.id.frag_container, new FragmentTwo());
+				break;
+
+			}
+			transaction.commit();
+		}
+	};
 }
